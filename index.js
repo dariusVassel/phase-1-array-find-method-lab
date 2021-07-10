6898e589c12b95aef0b002e7865f692d69057565
+ <+>
2021-07-10 19:49:05 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function superbowlWin(array){
+    let foundItem = (array.find(checker));
+    if (foundItem=== undefined) {
+    return;
+    } else {
+      return foundItem.year;
+}
+  console.log(typeof(foundItem))
+  console.log(foundItem);
+    
+}
 
+function checker(element, index, array){
+    return (element.result === "W")
+}
+
+
+superbowlWin(record);
